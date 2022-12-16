01e0d1c7d8670df6f27b71f1199015ab07c5a2e3
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-12-16 23:14:42 +0200

    feat: create BookShelf component

---
+++ starter/src/components/BookShelf.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Book from './Book';
+
+const BookShelf = ({ title, books, updateStore }) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map(({ id, title, shelf, authors, imageLinks }) => (
+            <li key={id}>
+              <Book
+                id={id}
+                key={id}
+                title={title}
+                shelf={shelf}
+                authors={authors}
+                updat={updateStore}
+                thumbnail={imageLinks.thumbnail}
+              />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default BookShelf;
