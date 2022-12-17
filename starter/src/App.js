03af0da501486f2dc46c2d61975053f285a4190f
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-12-17 11:43:49 +0200

    refactor: add not found route

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import SearchPage from './pages/search';
 import MainPage from './pages/main';
+import NotFound from './pages/not-found';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const App = () => {
@@ -12,7 +13,8 @@ const App = () => {
     {
       path: '/search',
       element: <SearchPage />
-    }
+    },
+    { path: '*', element: <NotFound /> }
   ]);
   return <RouterProvider router={router} />;
 };
